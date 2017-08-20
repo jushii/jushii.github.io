8ef76bb3137047b0235947618e8dff66647b6739
jushiip <jushii.tuomi@gmail.com>
2017-08-20 17:29:54 +0800

    added more projects, removed about page

---
+++ js/logic.js
@@ -0,0 +1,13 @@
+// $(document).ready(function () {
+//     $('wrapper').hover(function () {
+//         $(this).stop().animate({
+//             opacity: .4
+//         }, 200);
+//         $('.text').removeClass('hide');
+//     }, function () {
+//         $(this).stop().animate({
+//             opacity: 1
+//         }, 500);
+//         $('.text').addClass('hide');
+//     });
+// });
